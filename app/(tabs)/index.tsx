f72e531c147ebbe39daedc71d47b70b5ff698d95
Medrea Sergiu <msergiu2015@gmail.com>
2025-01-04 17:30:27 +0200

    merge

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,29 +6,31 @@ import React from "react";
 import { TaskProvider, useTasks } from "@/contexts/TaskProvider";
 import ProfileScreen from "@/components/ProfileScreen";
 import { UserProvider } from "../../contexts/UserContext";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <UserProvider>
-      <TaskProvider>
-        <Tab.Navigator
-          screenOptions={{
-            headerShown: false, // Hide the top header
-          }}
-        >
-          <Tab.Screen
-            name="Main"
-            component={MainScreen}
-            options={{
-              tabBarLabel: "Main",
-              tabBarIcon: ({ color, size }) => (
-                <TabBarIcon name="home" color={color} size={size} />
-              ),
+    <NavigationContainer>
+      <UserProvider>
+        <TaskProvider>
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false, // Hide the top header
             }}
-          />
-          {/* <Tab.Screen
+          >
+            <Tab.Screen
+              name="Main"
+              component={MainScreen}
+              options={{
+                tabBarLabel: "Main",
+                tabBarIcon: ({ color, size }) => (
+                  <TabBarIcon name="home" color={color} size={size} />
+                ),
+              }}
+            />
+            {/* <Tab.Screen
             name="Profile Creation"
             component={CreateAccount}
             options={{
@@ -38,28 +40,29 @@ export default function App() {
               ),
             }}
           /> */}
-          <Tab.Screen
-            name="Profile Screen"
-            component={ProfileScreen}
-            options={{
-              tabBarLabel: "Profile Screen",
-              tabBarIcon: ({ color, size }) => (
-                <TabBarIcon name="person" color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Calendar"
-            component={CalendarScreen}
-            options={{
-              tabBarLabel: "Calendar",
-              tabBarIcon: ({ color, size }) => (
-                <TabBarIcon name="calendar" color={color} size={size} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </TaskProvider>
-    </UserProvider>
+            <Tab.Screen
+              name="Profile Screen"
+              component={ProfileScreen}
+              options={{
+                tabBarLabel: "Profile Screen",
+                tabBarIcon: ({ color, size }) => (
+                  <TabBarIcon name="person" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Calendar"
+              component={CalendarScreen}
+              options={{
+                tabBarLabel: "Calendar",
+                tabBarIcon: ({ color, size }) => (
+                  <TabBarIcon name="calendar" color={color} size={size} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </TaskProvider>
+      </UserProvider>
+    </NavigationContainer>
   );
 }
